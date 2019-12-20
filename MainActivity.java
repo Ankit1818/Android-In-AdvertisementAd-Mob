4e380b49871136d709746c960bfed9bf26738fbf
Ankit Goswami <53818978+Ankit1818@users.noreply.github.com>
2019-12-20 11:07:42 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.advertisement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+        adView=findViewById(R.id.adView);
+        AdRequest adRequest=new AdRequest.Builder().addTestDevice("").build();
+        adView.loadAd(adRequest);
+
+    }
+}
